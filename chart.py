630c5979a37ae708c8bb6f2a26027abc0ec90c98
wodim <neikokz@gmail.com>
2013-11-27 12:17:22 +0100

    add queued users over online users

--- chart.py
+++ chart.py
@@ -9,8 +9,8 @@ from collections import OrderedDict
 from datetime import datetime
 
 realms = {
-    'WotLK': ('Lordaeron', 'Deathwing', 'Ragnaros'),
-    'Cataclysm': ('Frostwolf', 'Neltharion', 'Sargeras', 'Warsong'),
+    'WotLK': {'capacity': 4000, 'realms': ('Lordaeron', 'Deathwing', 'Ragnaros')},
+    'Cataclysm': {'capacity': 3500, 'realms': ('Frostwolf', 'Neltharion', 'Sargeras', 'Warsong')},
 }
 
 translations = {
@@ -35,7 +35,7 @@ html_charts = OrderedDict({})
 for version in realms.iteritems():
     # cata or wotlk
     users = {}
-    for realm_name in version[1]:
+    for realm_name in version[1]['realms']:
         # each one of the realms
         users[realm_name] = []
         cursor.execute('SELECT timestamp, users_online, users_queued FROM plot WHERE timestamp > ? AND realm = ?', (twodays, realm_name))
@@ -44,11 +44,24 @@ for version in realms.iteritems():
 
     # resort the list of dicts of tuples of lists of tuples of lists of lists
     store = OrderedDict({})
-    for realm_name in version[1]:
+    for realm_name in version[1]['realms']:
         for item in users[realm_name]:
             alt_key = datetime.fromtimestamp(item[0] + timediff).strftime('%d-%b %H:%M:%S')
             keylist = store.get(alt_key, [])
-            keylist.append((realm_name, item[1], item[2]))
+
+            users_online = item[1]
+            users_queued = item[2]
+            # if a realm is full or empty, don't draw a line for online users
+            if users_online >= version[1]['capacity'] or users_online == 0:
+                users_online = 'null'
+            # then, if there's a queue, sum the capacity of the realm to the queue and draw it
+            if users_online == 'null' and users_queued != 0:
+                users_queued += version[1]['capacity']
+            # but if there are no players and no queue, the realm is down, don't draw any lines
+            elif users_queued == 0:
+                users_queued = 'null'
+
+            keylist.append((realm_name, users_online, users_queued))
             store[alt_key] = keylist
 
     html_charts[version[0]] = store
